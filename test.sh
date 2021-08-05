2822b02157c44a845d637363797399549f095201
Anya Kuznetsova <anna.y.kuznetsova@gmail.com>
2021-08-05 00:40:06 -0700

    Add GPU image to test script

--- test.sh
+++ test.sh
@@ -9,3 +9,14 @@ docker run --volume $(pwd):/birdnet cmi/birdnet \
        --source-dir "/birdnet/" \
        --filename-formats "site.yyyymmddHHMMSS" \
        --survey-csv "/birdnet/test/survey.csv"
+
+
+docker run --volume $(pwd):/birdnet cmi/birdnet-gpu \
+       python3 analyze_batch.py \
+       --source-files "/birdnet/test/249BC3055C030FB4_20200308_181500_0.flac" "/birdnet/test/lala.flac" \
+       --dataset-path "/birdnet/test_outputs/birdnet" \
+       --model-tag-path "/birdnet/model/BirdNET_Soundscape_Model.pkl" \
+       --parameters '{"latitude": 36, "longitude": -121, "week": 24, "overlap": 0, "spp": 1, "sensitivity": 1.0, "min_conf": 0.000001}' \
+       --source-dir "/birdnet/" \
+       --filename-formats "site.yyyymmddHHMMSS" \
+       --survey-csv "/birdnet/test/survey.csv"
